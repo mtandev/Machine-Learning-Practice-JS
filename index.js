4fd03a8bcc49c5d2d2b26672768e6d1000c48c2f
Michelle T <32177770+mtandev@users.noreply.github.com>
2019-06-07 12:45:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ network.train([
     { input: [2, 4], output: [1]}, // Team 4 won
     { input: [1, 2], output: [0]}, // Team 1 won
     { input: [1, 3], output: [0]}, // Team 1 won
-    { input: [3, 4], output: [0]} // Team 3 won
+    { input: [3, 4], output: [0]}, // Team 3 won
+    { input: [1, 4], output: [1]} // Team 4 won
 ]);
 
 const output = network.run([1,4]);
